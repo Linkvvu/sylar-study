c792e6b65ab3bcc77240baf17c8285ac3edd4559
wuhao <wuhaocoding@163.com>
2024-06-09 06:42:08 +0800

    upgrade:     1. add singleton Config manager     2. config-var supports common STL containers

---
+++ sylar/config.cpp
@@ -0,0 +1,62 @@
+#include "config.h"
+
+using namespace sylar;
+
+bool sylar::AbsConfigVar::IsValidName(const std::string& name) {
+	std::regex pattern("^[a-zA-Z0-9._]+$");
+    return std::regex_match(name, pattern);
+}
+
+AbsConfigVar::AbsConfigVar(std::string name, std::string desc)
+	: name_(std::move(name))
+	, desc_(std::move(desc))
+{
+	if (!IsValidName(name_)) {
+		throw std::invalid_argument("invalid config_var name: " + name_);
+	}
+}
+
+static void ListAllNode(const YAML::Node& root, std::vector<std::pair<const std::string, const YAML::Node>>& node_set, const std::string& name = "") {
+	if (AbsConfigVar::IsValidName(name) || name.empty()) {
+		if (!name.empty()) {
+			node_set.emplace_back(name, root);
+		}
+		if (root.IsMap()) {
+			for (auto it = root.begin(); it != root.end(); ++it) {
+				ListAllNode(it->second, node_set, name.empty() ? it->first.Scalar() : name + '.' + it->first.Scalar());
+			}
+		}
+	} else {
+		throw std::invalid_argument("Failed to parse the yaml document, has unexpected character");
+	}
+}
+
+void sylar::Config::LoadFromFile(const char* path) {
+	YAML::Node doc_root = YAML::LoadFile(path);
+	std::vector<std::pair<const std::string, const YAML::Node>> node_set;
+
+	try {
+		ListAllNode(doc_root, node_set);
+	} catch (const std::exception& e) {
+		SYLAR_LOG_FMT_ERROR(SYLAR_SYS_LOGGER(), "Config::LoadFromFile exception: %s; filename=%s", e.what(), path);
+		return;
+	}
+
+	for (const auto& pair : node_set) {
+		std::shared_ptr<AbsConfigVar> cur_config_variable = FindConfigVarBase(pair.first);
+		if (cur_config_variable) {
+			if (pair.second.IsScalar()) {
+				cur_config_variable->FromString(pair.second.Scalar());
+			} else {
+				std::ostringstream oss;
+				oss << pair.second;
+				cur_config_variable->FromString(oss.str());
+			}
+		}
+	}
+}
+
+std::shared_ptr<AbsConfigVar> Config::FindConfigVarBase(const std::string& name) const {
+	auto it = configs_.find(name);
+	return it == configs_.end() ? nullptr : it->second;
+}
--- sylar/test/config_test.cpp
+++ sylar/test/config_test.cpp
@@ -1,5 +1,26 @@
 #include "../config.h"
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+class ConfigEnv : public testing::Test {
+public:
+	explicit ConfigEnv()
+		: config_(sylar::Singleton<sylar::Config>::GetInstance())
+		{}
+
+	virtual void SetUp() override {
+		std::set<double> tmp = {1.1, 2.1};
+		config_.AddOrUpdate<std::string>("person.name", "unknown name", "person's name");
+		config_.AddOrUpdate("person.age", 0, "person's age");
+		config_.AddOrUpdate<std::vector<std::string>>("person.email", {"test@163.com", "test@gmail.com"}, "person's email");
+		config_.AddOrUpdate<std::list<std::string>>("person.phone", {"110", "120", "911"}, "person's phone");
+		config_.AddOrUpdate<std::set<double>>("std.set", {1.1, 2.2, 3.3}, "");
+		config_.AddOrUpdate<std::map<int, std::string>>("std.map", {{1, "X"}, {2, "Y"}, {3, "Z"}}, "");
+	}
+
+protected:
+	sylar::Config& config_;
+};
 
 TEST(BasicLexicalCast, IntTest) {
 	sylar::ConfigVar<int> int_config_var("int", 0);
@@ -13,4 +34,21 @@ TEST(StlLexicalCast, VectorTest) {
 	ASSERT_EQ(vec_int_config_var.ToString(), "- 0\n- 10\n- 100");
 	vec_int_config_var.FromString("[1000, -1000, 9999999]");
 	ASSERT_EQ(vec_int_config_var.ToString(), "- 1000\n- -1000\n- 9999999");
+}
+
+TEST_F(ConfigEnv, LoadConfigsFromFile) {
+	config_.LoadFromFile("/home/wuhao/projs/sylar-study/sylar/test/config.yml");
+	EXPECT_EQ(config_.Find<std::string>("person.name")->GetValue(), "CXX");
+	EXPECT_EQ(config_.Find<int>("person.age")->GetValue(), 22);
+	EXPECT_THAT(config_.Find<std::vector<std::string>>("person.email")->GetValue(),
+		::testing::UnorderedElementsAre(std::string("wuhaocoding@163.com"), std::string("wuhaocoding@gmail.com")));
+	EXPECT_THAT(config_.Find<std::list<std::string>>("person.phone")->GetValue(),
+		::testing::UnorderedElementsAre(std::string("110"), "911", std::string("120")));
+	EXPECT_THAT(config_.Find<std::set<double>>("std.set")->GetValue(),
+		::testing::UnorderedElementsAre(10.10, 20.20));
+
+	auto map = config_.Find<std::map<int, std::string>>("std.map")->GetValue();
+	EXPECT_THAT(map, ::testing::ElementsAre(std::make_pair(1, std::string("XXX")),
+			std::make_pair(2, std::string("YYY")),
+			std::make_pair(3, std::string("ZZZ"))));
 }
